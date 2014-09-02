50d178999194c9b2ee24c339f86acf5f71d1072c
luizgabriel <luiz.gabriel.cristo@hotmail.com>
2014-09-01 22:29:05 -0300

    some changes to debuger

--- init.php
+++ init.php
@@ -11,8 +11,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
 
-ErrorHandler::register();
-ExceptionHandler::register();
+if ('cli' !== php_sapi_name()) {
+    ini_set('display_errors', 0);
+    ExceptionHandler::register();
+} elseif (!ini_get('log_errors') || ini_get('error_log')) {
+    ini_set('display_errors', 1);
+}
+ErrorHandler::register($errorReportingLevel);
  
 $request = Request::createFromGlobals();
 $response = new Response();
